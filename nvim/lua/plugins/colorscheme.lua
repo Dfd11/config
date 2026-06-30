dc4233f57fe913bba0b290e464e13fc3a9f1611a
dfd <davidfd2008@gmail.com>
2026-06-30 17:56:31 +0200

    Added ZSH, created files for bash and zsh

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  -- add gruvbox
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "warmer",
+      })
+      require("onedark").load()
+    end,
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+} -- Using Lazy
